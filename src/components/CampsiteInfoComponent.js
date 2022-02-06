39ccf8b7bac1a243b6e3f68092e1c9b1af0016e2
Teresa L <tml2009jghs@yahoo.com>
2022-02-05 19:35:59 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -40,8 +40,13 @@ class CommentForm extends React.Component {
     };
 
     handleSubmit = (values) => {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(
+            this.props.campsiteId,
+            values.rating,
+            values.author,
+            values.text
+        );
     };
 
     render() {
@@ -139,7 +144,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -156,7 +161,7 @@ function RenderComments({ comments }) {
                         }).format(new Date(Date.parse(comment.date)))}
                     </p>
                 ))}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -183,7 +188,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
