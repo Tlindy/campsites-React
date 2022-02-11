2bcd1a83394ed34701234216d8e5aee8d06c03f3
Teresa L <tml2009jghs@yahoo.com>
2022-02-10 23:16:00 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -43,7 +43,7 @@ class CommentForm extends React.Component {
 
     handleSubmit = (values) => {
         this.toggleModal();
-        this.props.addComment(
+        this.props.postComment(
             this.props.campsiteId,
             values.rating,
             values.author,
@@ -150,7 +150,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -167,7 +167,10 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         }).format(new Date(Date.parse(comment.date)))}
                     </p>
                 ))}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm
+                    campsiteId={campsiteId}
+                    postComment={postComment}
+                />
             </div>
         );
     }
@@ -216,7 +219,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
